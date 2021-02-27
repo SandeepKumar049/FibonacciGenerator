84302833685cbabf1fedb684a4e503f2d9dfdce5
SandeepKumar049 <79748347+SandeepKumar049@users.noreply.github.com>
2021-02-27 19:58:55 +0530

    Update FibonacciGenerator.cs
    Added new function

--- FibonacciGenerator.cs
+++ FibonacciGenerator.cs
@@ -3,16 +3,21 @@ using System;
    {  
      public static void Main(string[] args)  
       {  
-         int n1=0,n2=1,n3,i,number;    
+         int n1=0,n2=1,n3=1,number;    
          Console.Write("Enter the number of elements: ");    
          number = int.Parse(Console.ReadLine());  
-         Console.Write(n1+" "+n2+" ");    
-         for(i=2;i<number;++i)   
+         Console.Write(n1+" "+n2+" "); 
+         n3=fibonacciCalculator( n1,n2,n3,number);
+      }
+      public static int fibonacciCalculator(int n1, int n2, int n3,int number)
+      {
+         for(int i=2;i<number;++i)   
          {    
           n3=n1+n2;    
-          Console.Write(n3+" ");    
+          Console.Write(n3+" ");   
           n1=n2;    
           n2=n3;    
-         }    
+         } 
+         return n3;
       }  
    }  
